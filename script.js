26b630b161a60d376f36fba9c316872e9e7aeb2b
chegx <110959546+chegx@users.noreply.github.com>
2022-12-29 01:20:56 +0900

    Change menu visibility

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(".hamburger").click(function(){
-  $("ul").animate({right: 0});
+  $("ul").css("visibility", "visible").animate({right: 0});
   $("body").addClass("active");
 })
 
 $(".icon-close").click(function(){
-  $("ul").animate({right: '-260px'});
+  $("ul").css("visibility", "hidden").animate({right: '-260px'});
   $("body").removeClass("active");
 })
